ad251049af81cbb1ecc2df6b9e997fc740380d13
Kun Zhang <zhangk@codeaurora.org>
2010-08-04 11:59:05 -0700

    Merge v8_leading
    Set v8_leading as the default engine, and change the build options.
    
    Change-Id: I599f7e7081f1be45cda03cee0dd35d8bc232b0e6

--- Android.mk
+++ Android.mk
@@ -30,8 +30,16 @@ include $(CLEAR_VARS)
 # Build libv8 and v8shell
 # TODO: remove the check when the v8 build is fixed for x86
 ifeq ($(TARGET_ARCH),arm)
-    ENABLE_V8_SNAPSHOT = true
-    include $(BASE_PATH)/Android.mksnapshot.mk
-    include $(BASE_PATH)/Android.libv8.mk
-    include $(BASE_PATH)/Android.v8shell.mk
+    ifneq ($(TARGET_ARCH_VARIANT),armv5te-vfp)
+    	ENABLE_V8_SNAPSHOT = true
+    endif
+    ifeq ($(USE_DEFAULT_JS_ENGINE),true)
+        include $(BASE_PATH)/Android.mksnapshot.mk
+        include $(BASE_PATH)/Android.libv8.mk
+        include $(BASE_PATH)/Android.v8shell.mk
+    else
+        include $(BASE_PATH)/v8_leading/Android.mksnapshot.mk
+        include $(BASE_PATH)/v8_leading/Android.libv8.mk
+        include $(BASE_PATH)/v8_leading/Android.v8shell.mk
+    endif
 endif
