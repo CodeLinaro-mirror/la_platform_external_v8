4d0174b80e69541393b1140efc17e8449cd29d77
Ben Murdoch <benm@google.com>
2016-03-22 12:00:34 +0000

    Upgrade V8 to version 4.9.385.28
    Upgrade V8 to version 4.9.385.28
    
    https://chromium.googlesource.com/v8/v8/+/4.9.385.28
    
    Bug: 31987131
    Bug: 29178923
    
    Change-Id: I4b2e74289d4bf3667f2f3dc8aa2e541f63e26eb4
    (cherry picked from commit 014dc512cdd3e367bee49a713fdc5ed92584a3e5)

--- test/cctest/gay-precision.cc
+++ test/cctest/gay-precision.cc
@@ -100047,4 +100047,5 @@ Vector<const PrecomputedPrecision> PrecomputedPrecisionRepresentations() {
                                             number_elements);
 }
 
-} }  // namespace v8::internal
+}  // namespace internal
+}  // namespace v8
